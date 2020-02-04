691fd9226264f0d68b1708f425ed375133a84e5c
blucky36 <blucky36@gmail.com>
2020-02-03 20:10:24 -0700

    boi

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/boi', (req, res) =>  res.json( { 'uid': 1 } ))
 
 module.exports = router;
